ca902188788bbf636e6041b6ea149220f40531df
AditShah1234 <86316003+iamyesha@users.noreply.github.com>
2024-10-16 22:38:31 -0700

    Updated yolo
    Infer with Batch

--- src/model/yolo.py
+++ src/model/yolo.py
@@ -1,40 +1,65 @@
 import torch
 import sys
-sys.path.append('../')
-from util.gaussian_blur import gaussian_blur
-class YOLO():
-    def __init__(self,sub_model = "yolov5n",conf_threshold=.5):
-        self.model = torch.hub.load('ultralytics/yolov5',sub_model, pretrained=True)
-        self.model.classes = [2]# For Car
-        self.conf_threshold =  conf_threshold
-    
-    def infer(self,frame):
-        self.model.eval()
-        results = self.model(frame, size=640)
-        results = results.xyxy[0][results.xyxy[0][:, 4] > self.conf_threshold]
-        return results
-    
-    def infer_batch(self, image_list):
-        self.model.eval()
-        frame = [image[..., ::-1] for image in image_list]# to convert OpenCV image (BGR to RGB)
-        results = self.infer(frame)
-        self.results =results
-        return results
-    
-    def apply_gausian_blur(self,image_list,results):
-        if results ==None:
-            results = self.results
-        output =[]
-        for i, frame , results in enumerate(image_list,results):
-            car_boxes = []
-            for *box, conf, cls in results:
-                if int(cls) == 2: 
-                    car_boxes.append(box)
+import time
 
-            # Apply Gaussian blur on detected cars
-            if car_boxes:
-                frame = gaussian_blur.apply_gaussian_blur(frame, car_boxes)
-                output.append(frame)
-            
+sys.path.append("../")
+from util.gaussian_blur import gaussian_blur
+import logging
+
+logger = logging.getLogger(__name__)
+from tqdm import tqdm
+
+
+class YOLO:
+    def __init__(self, sub_model="yolov5n", conf_threshold=0.5):
+        self.model = torch.hub.load("ultralytics/yolov5", sub_model, pretrained=True)
+        self.model.classes = [2]  # For Car
+        self.conf_threshold = conf_threshold
+
+    def infer(self, frame_list):
+        self.model.eval()
+        frame = [image[..., ::-1] for image in frame_list]
+        results = self.model(frame, size=640)
+
+        return results
+
+    def infer_batch(self, image_list, batch_size=16):
+        self.model.eval()
+        results = []
+        start_time = time.time()
+        no_images = len(image_list)
+        for i in tqdm(range(0, no_images, batch_size)):
+            if i + batch_size > no_images - 1:
+                f = image_list[i:]
+            else:
+                f = image_list[i : i + batch_size]
+            r = self.infer(f)
+            results.append(r)
+        end_time = time.time()
+        self.results = results
+        logger.info(
+            "Avg time for prediction per frame done {}".format(
+                (end_time - start_time) / no_images
+            )
+        )
+        logger.info("Prediction done")
+        return results
+
+    def apply_gausian_blur(self, image_list, results):
+        if results == None:
+            results = self.results
+        output = []
+        for i in results:
+            for j in range(len(i.ims)):
+                frame, result = i.ims[j], i.xyxy[j]
+
+                car_boxes = []
+                for k in result:
+                    if k[4] > self.conf_threshold:
+                        car_boxes.append(k[:4])
+                if car_boxes:
+                    gb = gaussian_blur()
+                    frame = gb.apply_gaussian_blur(frame, car_boxes)
+                    output.append(frame)
+
         return output
-        
