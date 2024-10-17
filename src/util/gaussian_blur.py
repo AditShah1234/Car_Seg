3be7f5d29c46e9ca582b4f52ea4c972f1a49d5cd
AditShah1234 <86316003+iamyesha@users.noreply.github.com>
2024-10-17 00:23:50 -0700

    Update Gaussian Blur

--- src/util/gaussian_blur.py
+++ src/util/gaussian_blur.py
@@ -1,12 +1,14 @@
 import cv2
 
-class gaussian_blur():
+
+class gaussian_blur:
     def __init__(self) -> None:
         pass
-    def apply_gaussian_blur(self,image, boxes):
+
+    def apply_gaussian_blur(self, image, boxes):
         for box in boxes:
             x1, y1, x2, y2 = map(int, box)  # Bounding box coordinates
             car_roi = image[y1:y2, x1:x2]  # Extract region of interest (car)
-            blurred_car = cv2.GaussianBlur(car_roi, (51, 51), 0)  # Apply Gaussian blur
+            blurred_car = cv2.GaussianBlur(car_roi, (21, 21), 0)  # Apply Gaussian blur
             image[y1:y2, x1:x2] = blurred_car  # Replace car area with blurred version
         return image
